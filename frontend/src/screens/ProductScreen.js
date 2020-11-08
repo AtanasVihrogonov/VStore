a33adbd16b2383748fabc4ccbaf59cb241438d9c
av <vihrogonov@protonmail.com>
2020-11-08 09:36:16 +0000

    Create Product Screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,18 @@
+import { parseRequestUrl } from '../utils';
+import { getProduct } from '../api';
+
 // Create HomeScreen Object
 const ProductScreen = {
-  render: () => {
-    return `<div>ProductScreen</div>`;
-  }
+  render: async () => {
+    const request = parseRequestUrl();
+    const product = await getProduct(request.id);
+
+    if (product.error) {
+      return `<div>${product.error}</div>`;
+    }
+
+    return `<h1>${product.name}</h1>`;
+  },
 };
 
 export default ProductScreen;
