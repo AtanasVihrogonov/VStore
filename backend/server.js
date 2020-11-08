51b647b083ffa18a5b39f5601fc74056658efc17
av <vihrogonov@protonmail.com>
2020-11-08 20:17:01 +0000

    Connect to MongoDB and Create Admin User

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,29 @@
 import express from 'express';
 import cors from 'cors';
 import data from './data';
+import mongoose from 'mongoose';
+import config from './config';
+import userRouter from './routers/userRouter';
+
+// Connect to Database
+mongoose
+  .connect(config.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('Connected to mongodb.');
+  })
+  .catch((error) => {
+    console.log(error.reason);
+  });
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
+app.use('/api/users', userRouter);
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
@@ -19,7 +37,6 @@ app.get('/api/products/:id', (req, res) => {
     res.status(404).send({ message: 'Product Not Found!' });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}!`);
