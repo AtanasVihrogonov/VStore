465fcb7f14af8187b4dd82d4015976c65f23225e
av <vihrogonov@protonmail.com>
2020-11-08 11:56:16 +0000

    Added Product Screen UI

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,8 @@
 import { parseRequestUrl } from '../utils';
 import { getProduct } from '../api';
+import Rating from '../components/Rating';
 
-// Create HomeScreen Object
+// Create ProductScreen Object
 const ProductScreen = {
   render: async () => {
     const request = parseRequestUrl();
@@ -11,7 +12,55 @@ const ProductScreen = {
       return `<div>${product.error}</div>`;
     }
 
-    return `<h1>${product.name}</h1>`;
+    // Implement product screen HTML structure
+    return `
+      <div class="content">
+        <div class="back-to-result">
+          <i class="fa fa-caret-left" aria-hidden="true"></i>
+          <a href="/#/">Back to result</a>
+        </div>
+
+        <div class="details">
+          <div class="details-image">
+            <img src="${product.image}" alt="${product.name}"/>
+          </div>
+
+          <div class="details-info">
+            <ul>
+              <li>
+                <h1>${product.name}</h1>
+              </li>
+              <li>
+                ${Rating.render({
+                  value: product.rating,
+                  text: `${product.numReviews} reviews`,
+                })}
+              </li>
+              <li> 
+                Price: <strong>£${product.price}</strong>
+              </li>
+              <li>
+                Description: <div>${product.description}</div>
+              </li>
+            </ul>
+          </div>
+
+          <div class="details-action">
+            <ul class="fw">
+              <li>Price: £${product.price}</li>
+              <li>Status: 
+                  ${
+                    product.countInStock > 0
+                      ? `<span class="success">In Stock</span>`
+                      : `<span class="error">Unavailable</span>`
+                  }
+              </li>
+              <button id="add-button" class="primary fw">Add to Cart</button>
+            </ul>
+          </div>
+        </div>
+      </div>
+    `;
   },
 };
 
