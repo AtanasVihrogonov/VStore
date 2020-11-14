f3ff4ba8ed1fc7eb7f3651a841ffc168f7335dab
av <vihrogonov@protonmail.com>
2020-11-14 22:50:37 +0000

    Edit Product Backend(updateProduct, update productRouter only for admin user)

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -65,6 +65,29 @@ export const createProduct = async () => {
   }
 };
 
+// Create Update Product
+export const updateProduct = async (product) => {
+  try {
+    const { token } = getUserInfo();
+    const response = await axios({
+      url: `${apiUrl}/api/products/${product._id}`,
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      data: product
+    });
+    if (response.statusText !== 'OK') {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    return { error: err.response.data.message || err.message };
+  }
+};
+
+
 // Signin Function
 export const signin = async ({ email, password }) => {
   try {
